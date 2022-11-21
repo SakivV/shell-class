15321fe4c7ecaf17b85129023b59c245da8f9f10
Akash <phalke.aka@gmail.com>
2022-11-21 14:26:41 +0530

    shell script by akash

---
+++ newscript.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+BANK_NAME="AXIS BANK"
+echo "This is bank"
+echo "tell me your name"
+read FIRSTNAME
+echo "$BANK_NAME" have join account facility
+LOAN="AMOUNT"
+echo "$LOAN" is credited to "$BANK_NAME"
