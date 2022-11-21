72b08dcc533fcab578c51a0f810af6071db1592e
patilritesh1003 <113488423+patilritesh1003@users.noreply.github.com>
2022-11-21 14:41:32 +0530

    EMP_Data_Form

---
+++ Shel1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+CLASS_NAME="DevOps Engineer"
+echo "Welcome to DevOps Engineer"
+echo "Enter your first name:"
+read FIRSTNAME
+echo "Enter your last name:"
+read LASTNAME
+echo "Enter your JOB Role:"
+read JOBROLE
+echo "Enter your EMP_ID:"
+read EMP_ID
+echo "$CLASS_NAME Empolyee Name: - $FIRSTNAME"
+echo "$LASTNAME"
+echo "$JOBROLE"
+echo "$EMP_ID"
