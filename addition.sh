e89b02945f3e7324ae9afab924997d308c1f96b5
vikas <banagevikas@gmail.com>
2022-11-25 18:00:37 +0100

    Recap for demo

--- addition.sh
+++ addition.sh
@@ -5,4 +5,5 @@ TOTAL_SUM=`expr $FIRST_NUM + $SECOND_NUM`
 echo "=======Addition========"
 echo "Addition is $TOTAL_SUM"
 echo "New Push for Jenkins and Git Push Integratin"
+echo "Lets recap Github push and Jenkins integration"
 
