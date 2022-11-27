c6ccecf04a2d3dcd099012907d9216e7bc87457a
vikas <banagevikas@gmail.com>
2022-11-27 07:16:37 +0100

    modify operator

--- copy-file-s3.sh
+++ copy-file-s3.sh
@@ -3,13 +3,13 @@ FILE_EXTENSION=$1
 for FILE_NAME in /Users/vikasbanage/randomimages/*.$FILE_EXTENSION
 do
    echo $FILE_NAME
-   if [ $FILE_EXTENSION -eq "pdf" ]
+   if [ $FILE_EXTENSION = "pdf" ]
    then
         /usr/local/bin/aws s3 cp $FILE_NAME  s3://cloudmagic-pdf/
-   elif [ $FILE_EXTENSION -eq "csv" ]
+   elif [ $FILE_EXTENSION = "csv" ]
    then
         /usr/local/bin/aws s3 cp $FILE_NAME  s3://cloudmagic-csv/
-   elif [ $FILE_EXTENSION -eq "jpeg" ]
+   elif [ $FILE_EXTENSION = "jpeg" ]
    then
         /usr/local/bin/aws s3 cp $FILE_NAME  s3://clodumagic-images/
    else
